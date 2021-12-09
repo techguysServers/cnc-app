48a787921ff59fe2cde03455d4dbc8753107be1e
pepinouz <phil.pepin96@gmail.com>
2021-12-09 00:45:22 -0500

    added inspection table in equipment profil and inspection form

--- pages/equipments/[id].js
+++ pages/equipments/[id].js
@@ -1,9 +1,7 @@
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 
-import Button from "../../components/Button";
-import Search from "../../components/Search";
 import EquipmentTable from "../../components/equipment/EquipmentTable";
+import TableHeader from "../../components/TableHeader";
 
 function Equipment({ equipments }) {
   const router = useRouter();
@@ -18,21 +16,7 @@ function Equipment({ equipments }) {
         </div>
       </div>
       <br />
-      <header class="w-full shadow-lg bg-white dark:bg-gray-700 items-center h-16 rounded-2xl z-40">
-        <div class="relative z-20 flex flex-col justify-center h-full px-3 mx-auto flex-center">
-          <div class="relative items-center pl-1 flex w-full lg:max-w-68 sm:pr-2 sm:ml-0">
-            <div class="container relative left-0 z-50 flex w-3/4 h-auto h-full">
-              <Search/>
-            </div>
-            <div class="relative p-1 flex items-center justify-end w-1/4 ml-5 mr-4 sm:mr-0 sm:right-auto">
-              <Button
-                label="Ajouter"
-                onClick={() => router.push("form/add?id=" + id)}
-              />
-            </div>
-          </div>
-        </div>
-      </header>
+      <TableHeader onClick={() => router.push("form/add?id=" + id)} />
       <br />
       <EquipmentTable data={equipments} />
     </div>
